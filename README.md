661b72f8e781958ff7f6ca8db6c3cbce782bab7d
Orllewin <orllewin@fastmail.uk>
2023-02-18 11:13:56 +0000

    use pdc

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Playdate: wav2pda
 
-The Playdate compiler converts .wav audio to .pda Playdate Audio files. If you tell the compiler to compile just a wav without Playdate project (eg. `pdc music.wav`) it'll still do the conversion process and package the converted .pda inside a .pdx directory, this utility just does that process and tidies up the files afterwards.
+The Playdate compiler converts .wav audio to .pda Playdate Audio files. If you tell the compiler to compile just a wav without Playdate project (eg. `pdc music.wav`) it'll still do the conversion process and package the converted .pda inside a .pdx directory, this utility just does that process and tidies up the files afterwards: `wav2pda music.wav`
 
 The idea is you can edit samples on your computer then add the converted .pda to the data directory of [Tape Looper](https://orllewin.github.io/playdate/tape_looper/) or other future audio projects.
 
-Before I discovered the compiler does this easily for you anyway I'd written some code that naively swaps the wav header for a pda header I took from a converted file, this works fine but I'm not sure how reliable it is, I've left it in as an option in case the compilaer behaviour changes in the future. 
+Before I discovered the compiler does this easily for you I'd written some code that naively swaps the wav header for a pda header I took from a converted file, this works fine but I'm not sure how reliable it is, I've left it in as an option in case the compiler behaviour changes in the future: `wav2pda -o music.wav` 
--- bin/index.js
+++ bin/index.js
@@ -12,38 +12,58 @@ if(process.argv.length == 2){
   process.exit(1)
 }
 
-if(process.argv.length != 3){
-  log("Invaid arguments")
-  process.exit(1)
-}
+var usePDC = true
+var wavfile = process.argv[2]
 
-const wavfile = process.argv[2]
+log("Arg size: " + process.argv.length)
+process.argv.forEach(arg => log("Arg: " + arg))
+
+if(process.argv.length == 4){
+  if(process.argv[2] == "-o"){
+    usePDC = false
+    wavfile = process.argv[3]
+  }else{
+    log("Invaid arguments")
+    process.exit(1)
+  } 
+}
 
 if(!wavfile.endsWith(".wav")){
   log("Invaid argument, file must be a .wav")
   process.exit(1)
 }
 
-log("Converting " + wavfile)
+if(usePDC){
+  log("Converting " + wavfile + " using Playdate compiler")
+  execSync("pdc " + wavfile)
 
-const filebuffer = fs.readFileSync(wavfile)
-const wavbytes = Uint8Array.from(filebuffer)
+  const pdx = wavfile + ".pdx"
+  const pdafile = wavfile.replace(".wav", ".pda")
+  execSync("mv " + pdx + "/" + pdafile + " ./" + pdafile)
+  execSync("rmdir " + pdx)
 
-const percentbyte = (element) => element == 37//dec 37 == 0x25
-const percindex = wavbytes.findIndex(percentbyte)
-
-const wavdata = wavbytes.slice(percindex)
-const pdaprefix = Uint8Array.from([80, 108, 97, 121, 100, 97, 116, 101, 32, 65, 85, 68, 68, -84, 0, 2, 44, 0, -33, -1, -79, -1, -90, -1, -66, -1, -25, -1, 13, 0])
-const pdalength = pdaprefix.length + wavdata.length
-
-const pdabytes = new Uint8Array(pdalength)
-pdabytes.set(pdaprefix, 0)
-pdabytes.set(wavdata, pdaprefix.length)
-
-const pdafile = wavfile.replace(".wav", ".pda")
-fs.writeFileSync(pdafile, pdabytes)
-
-log("Ready: " + pdafile)
+  log("Ready: " + pdafile)
+}else{
+  log("Converting " + wavfile + " by swapping file header")
+  const filebuffer = fs.readFileSync(wavfile)
+  const wavbytes = Uint8Array.from(filebuffer)
+  
+  const percentbyte = (element) => element == 37//dec 37 == 0x25
+  const percindex = wavbytes.findIndex(percentbyte)
+  
+  const wavdata = wavbytes.slice(percindex)
+  const pdaprefix = Uint8Array.from([80, 108, 97, 121, 100, 97, 116, 101, 32, 65, 85, 68, 68, -84, 0, 2, 44, 0, -33, -1, -79, -1, -90, -1, -66, -1, -25, -1, 13, 0])
+  const pdalength = pdaprefix.length + wavdata.length
+  
+  const pdabytes = new Uint8Array(pdalength)
+  pdabytes.set(pdaprefix, 0)
+  pdabytes.set(wavdata, pdaprefix.length)
+  
+  const pdafile = wavfile.replace(".wav", ".pda")
+  fs.writeFileSync(pdafile, pdabytes)
+  
+  log("Ready: " + pdafile)
+}
 
 function log(message){
   console.log(message)
